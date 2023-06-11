bb9a5e136b5a92d943f854e6c00a9c24e3da75cc
hadesbaker <127382572+hadesbaker@users.noreply.github.com>
2023-06-10 22:19:23 -0700

    Add files via upload
    lab 3

---
+++ lab-03/lab-03-module.js
@@ -0,0 +1,27 @@
+// TODO Part 9: Add the reverseString function to the module.exports object
+// for import using require()
+const { reverseString, concatenateString } = require('lab-03-module.js');
+
+// TODO Part 10: Add an anonymous function concatenateString that takes a string
+// as a parameter, and returns the original concatenated to itself
+
+const concatenateString = function(str) {
+    return str + str;
+  };
+  
+module.exports = {
+    reverseString
+};
+
+// TODO Part 8: Reference the following website to use the spread operator
+// to reverse the string for reverseString(str) function. Comment out the
+// original return line of code.
+// https://betterprogramming.pub/5-ways-to-reverse-a-string-in-javascript-466f62845827
+
+function reverseString(str) {
+    return [...str].reverse().join('');
+  }
+  
+//function reverseString(str) {
+//    return str.split("").reverse().join("");
+//}
